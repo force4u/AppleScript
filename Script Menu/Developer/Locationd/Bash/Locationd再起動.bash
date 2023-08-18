751c08a349cd4c70ddd34c8ae72fded7c770347d
force4u <clubsmoky@gmail.com>
2023-08-19 01:39:59 +0900

    Update Locationd再起動.bash

--- Script Menu/Developer/Locationd/Bash/Locationd再起動.bash
+++ Script Menu/Developer/Locationd/Bash/Locationd再起動.bash
@@ -19,7 +19,19 @@ else
 	/bin/echo "実行したユーザー：" "$SUDO_USER"
 fi
 
+
 ########################################
+/bin/echo "----+----1----+----2----+-----3----+----4----+----5----+----6----+----7"
+###アクセス権メンテナンス
+STR_DIR_PATH="/private/var/db/locationd/Library/Preferences"
+/bin/echo "メンテナンスディレクトリ：$STR_DIR_PATH"
+STR_RESPONSE=$(/usr/bin/chgrp -R _locationd "$STR_DIR_PATH")
+/bin/echo "/usr/bin/chgrp -R _locationd $STR_DIR_PATH"
+/bin/echo "↑ 実行したコマンドライン ↓ 結果"
+/bin/echo "$STR_RESPONSE"
+
+########################################
+/bin/echo "locationdを再起動します"
 /bin/echo "----+----1----+----2----+-----3----+----4----+----5----+----6----+----7"
 ###
 STR_RESPONSE=$(/usr/bin/killall -HUP locationd)
