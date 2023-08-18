fbcc3f71a0f91f0897c0402467b30eac984390c4
force4u <clubsmoky@gmail.com>
2023-08-19 01:52:59 +0900

    Update clients設定確認.bash

--- Script Menu/Developer/Locationd/Clients/Bash/clients設定確認.bash
+++ Script Menu/Developer/Locationd/Clients/Bash/clients設定確認.bash
@@ -26,17 +26,39 @@ STR_DEVICE_UUID=$(/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/awk '/
 ###USERのUUID
 STR_USER_UUID=$(/usr/bin/dscl localhost -list /Local/Default/Users GeneratedUID | grep "$SUDO_USER" | /usr/bin/awk '/ / { print $2 }')
 /bin/echo "ユーザーUUID：" "$STR_USER_UUID"
-
 ########################################
 /bin/echo "----+----1----+----2----+-----3----+----4----+----5----+----6----+----7"
 ###notbackedup 分
+STR_DIR_PATH="/private/var/db/locationd/Library/Preferences/ByHost/"
+STR_FILE_NAME="com.apple.locationd.notbackedup.$STR_DEVICE_UUID.plist"
+STR_PLIST_PATH="$STR_DIR_PATH$STR_FILE_NAME"
+/bin/echo "PLISTファイルパス：$STR_PLIST_PATH"
+###Backup
+/usr/bin/sudo -u "$SUDO_USER"  /bin/mkdir -p "/Users/$SUDO_USER/Documents/Apple/Locationd"
+/usr/bin/ditto "$STR_PLIST_PATH" "/Users/$SUDO_USER/Documents/Apple/Locationd/$STR_FILE_NAME"
+###通常分
+STR_DIR_PATH="/private/var/db/locationd/Library/Preferences/ByHost/"
+STR_FILE_NAME="com.apple.locationd.$STR_DEVICE_UUID.plist"
+STR_PLIST_PATH="$STR_DIR_PATH$STR_FILE_NAME"
+/bin/echo "PLISTファイルパス：$STR_PLIST_PATH"
+###Backup
+/usr/bin/ditto "$STR_PLIST_PATH" "/Users/$SUDO_USER/Documents/Apple/Locationd/$STR_FILE_NAME"
+########################################
+###locationd
 STR_DIR_PATH="/private/var/db/locationd/"
 STR_FILE_NAME="clients.plist"
 STR_PLIST_PATH="$STR_DIR_PATH$STR_FILE_NAME"
 /bin/echo "PLISTファイルパス：$STR_PLIST_PATH"
+########################################
+###Backup
+/usr/bin/ditto "$STR_PLIST_PATH" "/Users/$SUDO_USER/Documents/Apple/Locationd/$STR_FILE_NAME"
+###アクセス権
+/usr/sbin/chown -R  "$SUDO_USER"  "/Users/$SUDO_USER/Documents/Apple/Locationd"
+/bin/chmod -R 755 "/Users/$SUDO_USER/Documents/Apple/Locationd"
+/bin/echo "PLISTバックアップ：/Users/$SUDO_USER/Documents/Apple/Locationd"
+########################################
+###
 STR_RESPONSE=$(/usr/bin/sudo -u _locationd /usr/bin/defaults read "$STR_PLIST_PATH")
-
-
 #	STR_CNT=$(/bin/echo "$STR_RESPONSE" | /usr/bin/tr -d -c '}' | /usr/bin/wc -m)
 STR_CNT=$(/bin/echo "$STR_RESPONSE" | /usr/bin/grep -o  '};' | /usr/bin/wc -l)
 /bin/echo "登録されているアプリケーション数:$STR_CNT"
@@ -86,6 +108,21 @@ ESCAPED_UUID_BUNDLEID="${ITEM_UUID_BUNDLEID//:/\\:}"
 /bin/echo "パス:$STR_BUNDLEPATH"
 done
 
+########################################
+/bin/echo "----+----1----+----2----+-----3----+----4----+----5----+----6----+----7"
+###アクセス権メンテナンス
+/bin/echo "アクセス権メンテナンス：$STR_PLIST_PATH"
+/usr/bin/chgrp -R _locationd "$STR_PLIST_PATH"
+/bin/echo "/usr/bin/chgrp -R _locationd $STR_PLIST_PATH"
+/bin/echo "↑ 実行したコマンド"
 
+/usr/sbin/chown -R _locationd "$STR_PLIST_PATH"
+/bin/echo "/usr/sbin/chown -R _locationd $STR_PLIST_PATH"
+/bin/echo "↑ 実行したコマンド"
+
+STR_DIR_PATH="/private/var/db/locationd"
+/usr/sbin/chown -R _locationd "$STR_DIR_PATH"
+/bin/echo "/usr/sbin/chown -R _locationd $STR_DIR_PATH"
+/bin/echo "↑ 実行したコマンド"
 
 exit 0
